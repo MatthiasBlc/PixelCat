c4815d9245278b734a8078bc706e7aa53aaed47e
Iris <marlx42@protonmail.com>
2022-06-06 13:08:22 +0200

    correction_syntaxe_model_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-validate :first_name, presence: true,
-validate :last_name, presence: true,
-validate :address_1, presence: true,
-validate :zip_code, presence: true,
-validate :city, presence: true,
-validate :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Veuillez saisir un email valide s'il vous plaît." },
-validate :encrypted_password, presence: true, lenght: { minimum: 6 },
+validate :first_name, presence: true
+validate :last_name, presence: true
+validate :address_1, presence: true
+validate :zip_code, presence: true
+validate :city, presence: true
+validate :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Veuillez saisir un email valide s'il vous plaît." }
+validate :encrypted_password, presence: true, lenght: { minimum: 6 }
 
 end
