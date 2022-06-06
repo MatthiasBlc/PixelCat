196d0f8af54d0e718cdb249ba35611fc52ea05bf
Iris <marlx42@protonmail.com>
2022-06-06 14:22:44 +0200

    delete coma on validates model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, presence: true,
-  validates :last_name, presence: true,
-  validates :address_1, presence: true,
-  validates :zip_code, presence: true,
-  validates :city, presence: true,
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Veuillez saisir un email valide s'il vous plaît." },
-  validates :encrypted_password, presence: true, lenght: { minimum: 6 },
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :address_1, presence: true
+  validates :zip_code, presence: true
+  validates :city, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Veuillez saisir un email valide s'il vous plaît." }
+  validates :encrypted_password, presence: true, lenght: { minimum: 6 }
 end
