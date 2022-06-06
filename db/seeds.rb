77cf8b6f674d40a63142c437be3de1b0681cd5cd
Fx Tarbouriech <fxtarbouriech@hotmail.fr>
2022-06-06 11:52:22 +0200

    adding seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+#10.times do
+#user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, adress_1: Faker::Address.street_address, adress_2: Faker::Address.secondary_address, postal_code: Faker::Address.zip_code, city: Faker::Address.city, email: Faker::Internet.email, password_encrypted: Faker::Internet.password)
+#end
+
+20.times do
+    item = Item.create!(title: Faker::Artist.name, description: Faker::Creature::Cat.breed, price: rand(1..250), img_url: Faker::LoremFlickr.unique.image(size: "320x240", search_terms: ['kitten'])
+end
+
