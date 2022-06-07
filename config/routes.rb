3a9fae9f5c331b259e612681b5d7e180cf7a32e9
MatthiasBlc <mat.bouloc@gmail.com>
2022-06-07 16:36:50 +0200

    temps 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'carts/index'
-  get 'carts/show'
+  # get 'carts/index'
+  # get 'carts/show'
   root to: 'items#index'
 
   devise_for :users
 
   resources :items, only: %i[index show]
+  resources :carts, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
