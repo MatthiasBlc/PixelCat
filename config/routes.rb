df4be090d32d3eeae6b2e3f5647af3b909f47fbc
Iris <marlx42@protonmail.com>
2022-06-07 14:48:27 +0200

    quick fix sign out

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  # devise_for :users do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
+
   resources :items, only: %i[index show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
