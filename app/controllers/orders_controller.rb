bd36f16107c54158459de7280fd611fc7a3ac2fe
MatthiasBlc <mat.bouloc@gmail.com>
2022-06-07 23:55:07 +0200

    create simple order work, need to work on jointable

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,7 +10,44 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.create()
+    @order = Order.create
 
+    # @event = Event.find(params[:event_id])
+    @user = current_user
+    @stripe_amount = params[:total_price]
+
+    # puts "*"*50
+
+    # puts "*"*50
+
+    @order = Order.create(id_stripe: 123,
+                          user: @user,
+                          date: DateTime.now,
+                          total_price: @stripe_amount,
+                          id_user: @user.id)
+
+    if @order.save
+
+
+
+      # add cart in jointable customer.id
+
+
+
+
+      # clear cart
+
+
+
+
+
+      redirect_to root_path
+      # redirect add cart in jointable customer.id
+
+
+    else
+      flash.now[:alert] = @order.errors.full_messages
+      render 'new'
+    end
   end
 end
