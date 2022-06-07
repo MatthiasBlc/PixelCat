6dc05a6dd0664e20abdaf371d2f8339fdcdc47ce
MatthiasBlc <mat.bouloc@gmail.com>
2022-06-07 17:06:50 +0200

    add delete and sum

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,4 @@
 class CartsController < ApplicationController
-
   def index
     @carts = Cart.all
   end
@@ -9,20 +8,19 @@ class CartsController < ApplicationController
   end
 
   def create
-      
-      puts "$"*60
-      puts params
-      puts "$"*60
+    puts '$' * 60
+    puts params
+    puts '$' * 60
     @item = Item.find(params[:item_id])
     @cart = Cart.create(user_id: current_user.id, item_id: @item.id)
 
-      puts "$"*60
-      puts @item.id
-      puts "$"*60
+    puts '$' * 60
+    puts @item.id
+    puts '$' * 60
 
-      puts "$"*60
-      puts current_user.id
-      puts "$"*60
+    puts '$' * 60
+    puts current_user.id
+    puts '$' * 60
 
     # if @cart.save
     #   redirect_back(fallback_location: root_path)
@@ -32,9 +30,20 @@ class CartsController < ApplicationController
     #   render 'new'
     # end
 
-      respond_to do |format|
-        format.html { redirect_to carts_path, notice: "Votre image a été ajouté au panier." }
-        format.js {}
-      end
+    respond_to do |format|
+      format.html { redirect_to carts_path, notice: 'Votre image a été ajouté au panier.' }
+      format.js {}
+    end
   end
+
+  def destroy
+    @cart = Cart.find(params[:id])
+    @cart.destroy
+
+    respond_to do |format|
+      format.html { redirect_to emails_url, notice: "Email was successfully destroyed." }
+      format.js {}
+    end
+  end
+
 end
