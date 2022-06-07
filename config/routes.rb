dc1d1c77fd328f57d67281c8809bdb7fbfc25340
Fx Tarbouriech <fxtarbouriech@hotmail.fr>
2022-06-07 20:25:18 +0200

    edit form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root to: 'items#index'
 
   devise_for :users
@@ -6,7 +7,7 @@ Rails.application.routes.draw do
   # devise_for :users do
   #   get '/users/sign_out' => 'devise/sessions#destroy'
   # end
-
+  resources :users, only: [:show, :new, :create, :edit, :update]
   resources :items, only: %i[index show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
