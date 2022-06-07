61d51abe782f4965506eac1e71b5bbd83c6593f2
Iris <marlx42@protonmail.com>
2022-06-07 12:21:57 +0200

    add relations

---
+++ db/migrate/20220607100919_add_relations_item_cart.rb
@@ -0,0 +1,7 @@
+class AddRelationsItemCart < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :carts, :user, foreign_key: true
+    add_reference :carts, :item, foreign_key: true
+    add_reference :orders, :user, foreign_key: true
+  end
+end
