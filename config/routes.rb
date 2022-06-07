30a39ba5c63d04653bfa50c621b142096b50dee3
Iris <marlx42@protonmail.com>
2022-06-07 16:26:57 +0200

    fix button add cart

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  # devise_for :users do
-  #   get '/users/sign_out' => 'devise/sessions#destroy'
-  # end
-
   resources :items, only: %i[index show]
+
+  resources :carts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
