ef50ef98223dd006d1cc8b8d2ea4d79eef4cf762
Iris <marlx42@protonmail.com>
2022-06-08 14:26:36 +0200

    start stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,25 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @orders = Orders.all
-  end
-
-  def new
-    @order = Order.new
-  end
-
   def create
-    @order = Order.create()
-
+    # Ici prévoir d'enregistrer la commande en base 
+    @stripe_amount = 500
+    begin
+      customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+      })
+      charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @stripe_amount,
+      description: "Achat sur la plateforme PixelCat",
+      currency: 'eur',
+      })
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_order_path
+    end
+    # Ici prévoir le delete le panier 
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   resources :items, only: %i[index show]
 
   resources :carts
+
+  resources :orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
