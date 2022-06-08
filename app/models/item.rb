8ceea06f678bc11fb7f848ca6e8d6d61c3fee6cd
Olivia <oliviarudolf67@gmail.com>
2022-06-08 22:43:55 +0200

    commiittt

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
 
   has_many :carts, dependent: :delete_all
-  has_one_attached :photo
+  has_one_attached :photo, service: :S3
   has_many :order_items
   has_many :orders, through: :order_items
 
